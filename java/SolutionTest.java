e0e801ed21482cccf331f204c520f1670112a6f0
chriswongatcuhk <wc38.wong@gmail.com>
2017-03-17 23:42:30 +0800

    My Java solution.

---
+++ java/SolutionTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.Arrays;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+	@Test
+	public void BasicTests() {
+		// assertEquals("expected", "actual");
+		int[] nValues = new int[] { 40, 250, 260, 230, 60 };
+		int[][] solutions = new int[][] { new int[] { 0, 0, 2 },
+				new int[] { 2, 1, 0 }, new int[] { 2, 0, 3 },
+				new int[] { 1, 1, 4 }, new int[] { 0, 0, 3 } };
+		for (int i = 0; i < nValues.length; i++)
+			assertEquals(Arrays.toString(solutions[i]),
+					Arrays.toString(Solution.withdraw(nValues[i])));
+	}
+}
